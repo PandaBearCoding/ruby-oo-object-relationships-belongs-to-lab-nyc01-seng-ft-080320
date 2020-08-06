308f0213fee8cd957c7b499ffd830acbc350cc56
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-08-06 13:51:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,16 +5,3 @@ class Artist
     @name = name
   end
 end 
-
-  
-  
-  
-  class Artist
-  attr_accessor :name, :genre
- 
-  def initialize(name, genre) 
-    @name = name
-    @genre = genre
-  end
- 
-end
